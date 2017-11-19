34ee3463cae3f010014303c903acf8119baf119c
rashendra <rashendra@gmail.com>
2017-11-20 05:57:16 +1100

    Test method changes !

--- src/test/java/com/coding/challenge/domain/EmployeeDetailsTest.java
+++ src/test/java/com/coding/challenge/domain/EmployeeDetailsTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class EmployeeDetailsTest {
 
 	@Test
-	public void shouldReturnTrueIfContactIsMeaningfullyEqual() {
+	public void shouldReturnTrueIfEmployeeDetailsIsMeaningfullyEqual() {
 		// given
 		EmployeeDetails emplyeeDetails = getEmployeeDetails(100l, "Alan", 150l);
 		EmployeeDetails emplyeeDetailsTwo = new EmployeeDetails(100l, "Alan", 150l);
@@ -18,9 +18,35 @@ public class EmployeeDetailsTest {
 		// then
 		Assertions.assertThat(isEqual).isTrue();
 	}
+	
+
+	
+	@Test
+	public void shouldReturnTrueIfEmployeeDetailsIsMeaningfullyEqual1() {
+		// given
+		EmployeeDetails emplyeeDetails = getEmployeeDetails(100l, "Alan", 150l);
+		EmployeeDetails emplyeeDetailsTwo = new EmployeeDetails(100l, "Alan", 150l);
+
+		// when
+		boolean isEqual = emplyeeDetails.equals(emplyeeDetailsTwo);
+		// then
+		Assertions.assertThat(isEqual).isTrue();
+	}
+	@Test
+	public void shouldReturnFalseIfEmployeeDetailsIsNotMeaningfullyEqual() {
+
+		// given
+		EmployeeDetails emplyeeDetails = getEmployeeDetails(101l, "Alan", 150l);
+		EmployeeDetails emplyeeDetailsTwo = new EmployeeDetails(100l, "Alan", 150l);
+
+		// when
+		boolean isEqual = emplyeeDetails.equals(emplyeeDetailsTwo);
+		// then
+		Assertions.assertThat(isEqual).isFalse();
+	}
 
 	@Test
-	public void shouldReturnFalseIfContactIsNotMeaningfullyEqual() {
+	public void shouldReturnFalseIfEmployeeDetailsIsNotMeaningfullyEqual1() {
 
 		// given
 		EmployeeDetails emplyeeDetails = getEmployeeDetails(101l, "Alan", 150l);
